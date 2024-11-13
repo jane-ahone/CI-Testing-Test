2006ef60e6f6b7a61fc37fa1ed6e88b83fbedd7e
Jane Ahone <jane.eloundou@gmail.com>
2024-11-13 16:47:32 +0100

    add error 500

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify(message="Hello level 400 FET, Quality Assurance!")
+    return jsonif(message="Hello level 400 FET, Quality Assurance!")
 
 
 if __name__ == '__main__':
