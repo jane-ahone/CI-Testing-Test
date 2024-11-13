4e77905554d7f843e96681d53fdeef48dddc421d
Jane Ahone Eloundou <jane.eloundou@gmail.com>
2024-11-13 16:53:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonif(message="Hello level 400 FET, Quality Assurance!")
+    return jsonify(message="Hello level 400 FET, Quality Assurance!")
 
 
 if __name__ == '__main__':
